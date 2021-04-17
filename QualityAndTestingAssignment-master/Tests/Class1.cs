2c0adbf4b97c8c8bbb375a6a67b4aa7f51c6c3da
k-mcdonald21 <60354522+k-mcdonald21@users.noreply.github.com>
2021-04-17 16:36:43 +0100

    Add files via upload

---
+++ QualityAndTestingAssignment-master/Tests/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using QualityAndTestingAssignment;
+
+namespace Tests
+{
+    [TestFixture]
+    public class InsuranceTests
+    {
+        [Test]
+        public void Test1()
+        {
+
+            Assert.That(InsuranceService.CalcPremium(20, "female"), Is.EqualTo(5.0));
+        }
+        [Test]
+        public void Test2()
+        {
+
+            Assert.That(InsuranceService.CalcPremium(60, "female"), Is.EqualTo(.375));
+        }
+        [Test]
+        public void Test3()
+        {
+
+            Assert.That(InsuranceService.CalcPremium(15, "female"), Is.EqualTo(0.0));
+        }
+        [Test]
+        public void Test4()
+        {
+
+            Assert.That(InsuranceService.CalcPremium(20, "male"), Is.EqualTo(6.0));
+        }
+        [Test]
+        public void Test5()
+        {
+
+            Assert.That(InsuranceService.CalcPremium(60, "male"), Is.EqualTo(.75));
+        }
+        [Test]
+        public void Test6()
+        {
+
+            Assert.That(InsuranceService.CalcPremium(15, "male"), Is.EqualTo(0.0));
+        }
+        [Test]
+        public void Test7()
+        {
+
+            Assert.That(InsuranceService.CalcPremium(420, "horse"), Is.EqualTo(0.0));
+        }
+        [Test]
+        public void Test8()
+        {
+
+            Assert.That(InsuranceService.CalcPremium(4, "horse"), Is.EqualTo(0.0));
+        }
+
+    }
+}
